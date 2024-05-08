c8fd80e0dd7cf32f58977d2b57747212d09d127c
wellreel <48851737+wellreel@users.noreply.github.com>
2024-05-07 20:05:34 -0400

    Initial Commit

---
+++ OfficeQuotes/OfficeQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfficeQuotesApp.swift
+//  OfficeQuotes
+//
+//  Created by Matthew Hundley on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct OfficeQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
