5fe10d13f68bd78929b8c3c0229fbf801e22c823
wellreel <48851737+wellreel@users.noreply.github.com>
2025-05-07 15:41:15 -0400

    .

--- QuotesWidget/QuotesWidget.swift
+++ QuotesWidget/QuotesWidget.swift
@@ -59,11 +59,12 @@ struct QuotesWidgetEntryView : View {
                     .fixedSize(horizontal: false, vertical: true)
                     .frame(maxHeight: 200)
                     .lineLimit(6)
-                    
+                    .shadow(radius: 15)
                 Divider()
                 Text(entry.character)
+                    .shadow(radius: 15)
             }
-            .shadow(radius: 10)
+            .shadow(radius: 15)
     }
 }
 
